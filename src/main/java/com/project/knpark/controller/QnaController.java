7793a9bbb2b7f3b8d1bae3bf8a566168eb211c88
Jeonghoike <jhi6334@naver.com>
2023-11-24 18:20:43 +0900

    qna ai

--- src/main/java/com/project/knpark/controller/QnaController.java
+++ src/main/java/com/project/knpark/controller/QnaController.java
@@ -38,8 +38,11 @@ public class QnaController {
 	}
 	@RequestMapping(value = "qnaDetail", method= {RequestMethod.GET, RequestMethod.POST})
 	public String qnaDetail(int qno, Model model, String after, int qgroup) {
+		System.out.println(1);
 		model.addAttribute("qna", qnaService.qnaDetail(qno, after));
+		System.out.println(2);
 		model.addAttribute("reply", qnaService.qnaAdminReply(qgroup));
+		System.out.println(3);
 		return "board/qnaDetail";
 	}
 	@RequestMapping(value = "qnaModify", method=RequestMethod.GET)
